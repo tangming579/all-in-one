1e8a942a00b62dca0551610d02557af7f05752c3
Tangming <infly123@sina.com>
2019-03-02 10:16:42 +0800

    gRPC Demo

---
+++ src/gRPC/gRPCDemo/gRPCServer/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gRPCServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
