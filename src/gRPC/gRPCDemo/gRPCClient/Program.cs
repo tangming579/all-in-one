f1eb10cf96c261a65779464bbad53a4de5e72a9f
Tangming <infly123@sina.com>
2019-03-02 10:44:20 +0800

    grpc

--- src/gRPC/gRPCDemo/gRPCClient/Program.cs
+++ src/gRPC/gRPCDemo/gRPCClient/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grpc.Core;
+using GRPCDemo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,15 @@ namespace gRPCClient
     {
         static void Main(string[] args)
         {
+            Channel channel = new Channel("127.0.0.1:9007", ChannelCredentials.Insecure);
+
+            var client = new gRPC.gRPCClient(channel);
+            var reply = client.SayHello(new HelloRequest { Name = "Infly" });
+            Console.WriteLine("来自" + reply.Message);
+
+            channel.ShutdownAsync().Wait();
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
         }
     }
 }
--- src/gRPC/gRPCDemo/gRPCServer/Program.cs
+++ src/gRPC/gRPCDemo/gRPCServer/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Grpc.Core;
+using GRPCDemo;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +8,33 @@ using System.Threading.Tasks;
 
 namespace gRPCServer
 {
+    class gRPCImpl : gRPC.gRPCBase
+    {
+        // 实现SayHello方法
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply { Message = "你好 " + request.Name + "，我是Server" });
+        }
+    }
+
     class Program
     {
-        static void Main(string[] args)
+        const int Port = 9007;
+
+        public static void Main(string[] args)
         {
+            Server server = new Server
+            {
+                Services = { gRPC.BindService(new gRPCImpl()) },
+                Ports = { new ServerPort("localhost", Port, ServerCredentials.Insecure) }
+            };
+            server.Start();
+
+            Console.WriteLine("gRPC server listening on port " + Port);
+            Console.WriteLine("任意键退出...");
+            Console.ReadKey();
+
+            server.ShutdownAsync().Wait();
         }
     }
 }
