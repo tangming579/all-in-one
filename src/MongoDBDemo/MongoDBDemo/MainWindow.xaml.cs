ba5f075e2a2dc0fe4265cf5bf587a7c46a393673
Tangming <infly123@sina.com>
2019-03-06 15:04:17 +0800

    MongoDB

--- src/MongoDBDemo/MongoDBDemo/MainWindow.xaml.cs
+++ src/MongoDBDemo/MongoDBDemo/MainWindow.xaml.cs
@@ -22,30 +22,32 @@ namespace MongoDBDemo
     /// </summary>
     public partial class MainWindow : Window
     {
-        private IMongoCollection<BsonDocument> MyConnection;
+        private IMongoCollection<col> MyConnection;
+        private MongoDBHelper helper = new MongoDBHelper();
 
         public MainWindow()
         {
             InitializeComponent();
             MyConnection = GetConnection();
+            //helper.Get()
         }
 
-        public IMongoCollection<BsonDocument> GetConnection()
+        public IMongoCollection<col> GetConnection()
         {
             // or use a connection string
             var client = new MongoClient("mongodb://localhost:27017");
             var database = client.GetDatabase("test");
-            var collection = database.GetCollection<BsonDocument>("col");
+            var collection = database.GetCollection<col>("col");
             return collection;
         }
 
         public string Query()
         {
-            var documents = MyConnection.Find(new BsonDocument()).ToList();
+            var documents = MyConnection.AsQueryable().ToList();
             var sb = new StringBuilder();
             foreach (var doc in documents)
             {
-                sb.Append(doc.ToJson().ToString());
+                sb.Append(doc.ToString());
             }
             return sb.ToString();
         }
@@ -53,6 +55,40 @@ namespace MongoDBDemo
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             txb.Text = Query();
+        }
+
+        public class col
+        {
+            public ObjectId id { get; set; }
+            public string title { get; set; }
+            public string description { get; set; }
+            public string by { get; set; }
+            public string url { get; set; }
+            public string[] tags { get; set; }
+            public int likes { get; set; }
+
+            public override string ToString()
+            {
+                return $"'title:{title} description:{description} url:{url}"+'\n';
+            }
+            public override int GetHashCode()
+            {
+                return base.GetHashCode();
+            }
+        }
+
+        private void btnInsert_Click(object sender, RoutedEventArgs e)
+        {
+            var col = new col()
+            {
+                title = "测试" + Guid.NewGuid().ToString().Replace("-", ""),
+                description = "这是描述信息",
+                by = "by",
+                url = "www.github.com",
+                likes = 10
+            };
+
+            MyConnection.InsertOne(col);
         }
     }
 }
