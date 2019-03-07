54837fb5097f444a6b340ba1f890a66eeda9143b
Tangming <infly123@sina.com>
2019-03-07 19:35:39 +0800

    RabbitMq

---
+++ src/MQ/RabbitmqCSharp/RabbitmqCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitmqCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
