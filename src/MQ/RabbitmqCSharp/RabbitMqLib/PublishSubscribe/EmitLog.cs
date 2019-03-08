7d45cb4c46ee252de0cf845ec61d5a9a1220491e
Tangming <infly123@sina.com>
2019-03-08 15:24:41 +0800

    PublishSubscribe

---
+++ src/MQ/RabbitmqCSharp/RabbitMqLib/PublishSubscribe/EmitLog.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqLib.PublishSubscribe
+{
+    public class EmitLog
+    {
+        public static void Start()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "154.8.184.140";
+            factory.UserName = "admin";//用户名
+            factory.Password = "admin";//密码
+            factory.Port = 5672;
+            factory.VirtualHost = "/";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var message = GetMessage(new string[] { "Hello" });
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "logs",
+                                     routingKey: "",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0)
+                   ? string.Join(" ", args)
+                   : "info: Hello World!");
+        }
+    }
+}
---
+++ src/MQ/RabbitmqCSharp/RabbitMqLib/PublishSubscribe/ReceiveLogs.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqLib.PublishSubscribe
+{
+    public class ReceiveLogs
+    {
+        public static void Start()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "154.8.184.140";
+            factory.UserName = "admin";//用户名
+            factory.Password = "admin";//密码
+            factory.Port = 5672;
+            factory.VirtualHost = "/";
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                  exchange: "logs",
+                                  routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
