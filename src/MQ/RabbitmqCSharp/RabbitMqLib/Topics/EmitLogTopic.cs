9b446f7d71400bc8c3e02679371cd7a7a034f201
Tangming <infly123@sina.com>
2019-03-09 10:48:09 +0800

    Topics

---
+++ src/MQ/RabbitmqCSharp/RabbitMqLib/Topics/EmitLogTopic.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMqLib.Topics
+{
+    public class EmitLogTopic
+    {
+        public void Start()
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "154.8.184.140";
+            factory.UserName = "admin";//用户名
+            factory.Password = "admin";//密码
+            factory.Port = 5672;
+            factory.VirtualHost = "/";
+
+            var args = new string[] { "Hello" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "topic_logs",
+                                        type: "topic");
+
+                var routingKey = (args.Length > 0) ? args[0] : "anonymous.info";
+                var message = (args.Length > 1)
+                              ? string.Join(" ", args.Skip(1).ToArray())
+                              : "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+                channel.BasicPublish(exchange: "topic_logs",
+                                     routingKey: routingKey,
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent '{0}':'{1}'", routingKey, message);
+            }
+        }
+    }
+}
