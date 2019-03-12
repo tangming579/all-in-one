d7abcfe9370f82493f23303e5066caa441e84405
Tangming <infly123@sina.com>
2019-03-12 08:28:22 +0800

    NIO

---
+++ src/NIO/src/main/java/com/tm/niolearn/demo/basic/FileChannelLearn.java
@@ -0,0 +1,18 @@
+package com.tm.niolearn.demo.basic;
+
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class FileChannelLearn {
+    public void readFile() throws Exception{
+        RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+        FileChannel fileChannel = aFile.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(48);
+        int byteRead = fileChannel.read(byteBuffer);
+        while (byteRead!=-1){
+
+        }
+    }
+}
