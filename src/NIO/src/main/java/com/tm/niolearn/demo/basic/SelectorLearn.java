bce67cd06d983f4d8595ee7daf2699e0e8d990a0
Tangming <infly123@sina.com>
2019-03-15 19:29:02 +0800

    Update SelectorLearn.java

--- src/NIO/src/main/java/com/tm/niolearn/demo/basic/SelectorLearn.java
+++ src/NIO/src/main/java/com/tm/niolearn/demo/basic/SelectorLearn.java
@@ -1,12 +1,41 @@
 package com.tm.niolearn.demo.basic;
 
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
 
 public class SelectorLearn {
 
+    int port = 8000;
     public  void ioSelector() throws IOException {
         Selector selector = Selector.open();
+        ServerSocketChannel channel = ServerSocketChannel.open();
+        channel.configureBlocking(false);
+        channel.socket().bind(new InetSocketAddress(port));
 
+        while (true){
+            int readyChannels = selector.select();
+            if(readyChannels == 0) continue;
+            Set<SelectionKey> selectionKeys = selector.selectedKeys();
+            Iterator<SelectionKey> keyIterator = selectionKeys.iterator();
+            while (keyIterator.hasNext()){
+                SelectionKey key = keyIterator.next();
+                if(key.isAcceptable()){
+
+                }else if(key.isConnectable()){
+
+                }else if(key.isReadable()){
+
+                }else if(key.isWritable()){
+
+                }
+                keyIterator.remove();
+            }
+        }
     }
 }
