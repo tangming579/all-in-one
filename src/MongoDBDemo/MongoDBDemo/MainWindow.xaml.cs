8544048cc360ad83bc5c774458afa916303815b2
Tangming <infly123@sina.com>
2019-04-12 11:32:35 +0800

    update MongoDB

--- src/MongoDBDemo/MongoDBDemo/MainWindow.xaml.cs
+++ src/MongoDBDemo/MongoDBDemo/MainWindow.xaml.cs
@@ -25,29 +25,53 @@ namespace MongoDBDemo
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
             MyConnection = GetConnection();
+            var databases = ListDatabase();
+            foreach(var db in databases)
+            {
+                txb.AppendText(db.ToString()+'\n');
+            }
         }
 
         private IMongoCollection<col> MyConnection;
+        //建议将MongoClient实例存储在全局位置，
+        //要么作为静态变量，要么存储在具有单例生命周期的IoC容器中。
+        //---MongoDB官方文档
+        public static MongoClient Client { private set; get; }
         private MongoDBHelper helper = new MongoDBHelper("mongodb://localhost:27017", "test");
 
         public IMongoCollection<col> GetConnection()
         {
-            var client = new MongoClient("mongodb://localhost:27017");
-            var database = client.GetDatabase("test");
+            //var client = new MongoClient("mongodb://host:27017,host2:27017/?replicaSet=rs0");
+            Client = new MongoClient("mongodb://localhost:27017");
+            var database = Client.GetDatabase("test");
             var collection = database.GetCollection<col>("col");
             return collection;
         }
-
+        public List<BsonDocument> ListDatabase()
+        {
+            using(var cursor = Client.ListDatabases())
+            {
+                var list = cursor.ToList();
+                return list;
+            }
+        }
         public List<col> SelectAll()
         {
-            var list = MyConnection.Find("{}").ToList();
+            ////Filter方式
+            //var list = MyConnection.Find("{}").ToList();
+            //return list;
+
+            //Lambda方式
+            var list = MyConnection.AsQueryable().ToList();
             return list;
         }
 
         public List<col> Select()
         {
-            ////Filter方式
+            ////Filter方式1
             //var filter = Builders<col>.Filter.Eq("by", "菜鸟教程");
+            ////Filter方式2
+            var filter = Builders<col>.Filter.Eq(x => x.by, "菜鸟教程");
             //var list = MyConnection.Find(filter).ToList();
             //return list;
 
@@ -61,14 +85,6 @@ namespace MongoDBDemo
             var query = MyConnection.AsQueryable().Where(x => x.by == "菜鸟教程").ToList();
             return query;
         }
-
-        public col SelectLinQ()
-        {
-            var result = MyConnection.AsQueryable()
-                .Where(x => x.by == "菜鸟教程")
-                .FirstOrDefault();
-            return result;
-        }
         // SelectMany、Skip、Take内嵌数组排序分页
         public List<col> SelectPage()
         {
@@ -81,10 +97,11 @@ namespace MongoDBDemo
                 .Where(s => s.description == "更新了描述信息")
                 .OrderBy(r => r.lastModified)
                 .Skip((index - 1) * size)
-                .Take(size)
-                .ToList();
+                .Take(size);
 
-            return page;
+            //var page1 = IAsyncCursorSourceExtensions.ToList(page);
+
+            return page.ToList();
         }
         public void Insert()
         {
@@ -102,21 +119,23 @@ namespace MongoDBDemo
             //MyConnection.InsertMany(list);
         }
 
-        public void Delete()
+        public long Delete()
         {
             var filter = Builders<col>.Filter.Eq("by", "by");
 
-            MyConnection.DeleteOne(filter);
+            var result = MyConnection.DeleteOne(filter);
+            return result.DeletedCount;
             //MyConnection.DeleteMany(filter);
         }
 
-        public void Update()
+        public long Update()
         {
             var filter = Builders<col>.Filter.Eq("by", "by");
             var list = new List<col>();
             var update = Builders<col>.Update.Set("by", "更新了by").Set("description", "更新了描述信息").CurrentDate("lastModified");
-            MyConnection.UpdateOne(filter, update);
-            MyConnection.UpdateMany(filter, update);
+            var result = MyConnection.UpdateOne(filter, update);
+            return result.ModifiedCount;
+            //MyConnection.UpdateMany(filter, update);
         }
 
         private void btnInsert_Click(object sender, RoutedEventArgs e)
