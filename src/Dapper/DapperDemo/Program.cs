4a7b2dbc9f3c49c75ee19a7387ac7bcee9cda65a
Tangming <infly123@sina.com>
2019-04-17 10:11:11 +0800

    Dapper

--- src/Dapper/DapperDemo/Program.cs
+++ src/Dapper/DapperDemo/Program.cs
@@ -10,11 +10,14 @@ namespace DapperDemo
 {
     class Program
     {
+        public const string connectionString = "server=127.0.0.1;database=test;uid=root;pwd=123456;charset=utf8;";
+
         static void Main(string[] args)
         {
-            MySqlConnection con = new MySqlConnection("server=127.0.0.1;database=test;uid=root;pwd=123456;charset=utf8;");
+
+            MySqlConnection con = new MySqlConnection(connectionString);
             //新增数据
-            con.Execute("insert into user values(null, '测试', '18312345678', '河北','test')");
+
             ////新增数据返回自增id
             var id = con.QueryFirst<int>("insert into user values(null, '测试2', '18312345678', '河北','test2');select last_insert_id();");
             ////修改数据
@@ -36,6 +39,48 @@ namespace DapperDemo
             Console.ReadKey();
         }
 
+        public static int Insert()
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                var result = con.Execute("insert into user values(null, '测试', '18312345678', '河北','test')");
+                return result;
+            }
+        }
+
+        public static int InsertMany()
+        {
+            var usersList = Enumerable.Range(0, 10).Select(i => new User()
+            {
+                id = Guid.NewGuid().ToString(),
+                name = "user" + i,
+                username = "批量插入" + i,
+            });
+
+            using (var con = new MySqlConnection(connectionString))
+            {
+                var result = con.Execute("insert into user values(@id,@name,@username)", usersList);
+                return result;
+            }
+        }
+
+        public static IEnumerable<User> Select(string userName, string phone)
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                var result = con.Query<User>("select * from user where username=@UserName and phone=Phone", new { UserName = userName, Phone = phone }); ;
+                return result;
+            }
+        }
+
+        public static int Update(string id, string userName)
+        {
+            using (var con = new MySqlConnection(connectionString))
+            {
+                var result = con.Execute("update user set UserName = @UserName where Id = @Id", new { UserName = userName, Id = id });
+                return result;
+            }
+        }
         public class User
         {
             public string id { get; set; }
