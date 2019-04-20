6e2f8d08c85f88af482361f89d5ca367e86ee141
Tangming <infly123@sina.com>
2019-04-20 11:01:06 +0800

    update

--- src/Redis/Infly.Cache.Redis/RedisHelper.cs
+++ src/Redis/Infly.Cache.Redis/RedisHelper.cs
@@ -44,5 +44,43 @@ namespace Infly.Cache.Redis
                 return redis.Set(key, t, timeSpan);
             }
         }
+
+        public static bool Set<T>(string key, T t, DateTime dateTime)
+        {
+            using (var redis = prcm.GetClient())
+            {
+                return redis.Set(key, t, dateTime);
+            }
+        }
+
+        public static bool Remove(string key)
+        {
+            using (var redis = prcm.GetClient())
+            {
+                return redis.Remove(key);
+            }
+        }
+
+        public static void FlushAll(string key)
+        {
+            using (var redis = prcm.GetClient())
+            {
+                redis.FlushAll();
+            }
+        }
+
+        public static void AddList<T>(string key,T t)
+        {
+            using (var redis = prcm.GetClient())
+            {
+                var redisTypedClient = redis.As<T>();
+                redisTypedClient.AddItemToList(redisTypedClient.Lists[key], t);
+            }
+        }
+
+        public static void RemoveList<T>(string key,T t)
+        {
+
+        }
     }
 }
