cd1a1ee2b089adc9c8121ca52be174d19a53fa28
Tangming <infly123@sina.com>
2019-04-20 11:20:26 +0800

    update

--- src/Redis/Infly.Cache.Redis/RedisHelper.cs
+++ src/Redis/Infly.Cache.Redis/RedisHelper.cs
@@ -31,56 +31,119 @@ namespace Infly.Cache.Redis
 
         public static bool Set<T>(string key, T t)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                return redis.Set(key, t);
+                return client.Set(key, t);
             }
         }
 
         public static bool Set<T>(string key, T t, TimeSpan timeSpan)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                return redis.Set(key, t, timeSpan);
+                return client.Set(key, t, timeSpan);
             }
         }
 
         public static bool Set<T>(string key, T t, DateTime dateTime)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                return redis.Set(key, t, dateTime);
+                return client.Set(key, t, dateTime);
             }
         }
 
         public static bool Remove(string key)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                return redis.Remove(key);
+                return client.Remove(key);
             }
         }
 
         public static void FlushAll(string key)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                redis.FlushAll();
+                client.FlushAll();
             }
         }
 
-        public static void AddList<T>(string key,T t)
+        public static void AddList<T>(string key, T t)
         {
-            using (var redis = prcm.GetClient())
+            using (var client = prcm.GetClient())
             {
-                var redisTypedClient = redis.As<T>();
-                redisTypedClient.AddItemToList(redisTypedClient.Lists[key], t);
+                var clientTypedClient = client.As<T>();
+                clientTypedClient.AddItemToList(clientTypedClient.Lists[key], t);
             }
         }
 
-        public static void RemoveList<T>(string key,T t)
+        public static bool RemoveList<T>(string key, T t)
         {
+            using (var client = prcm.GetClient())
+            {
+                var clientTypedClient = client.As<T>();
+                return clientTypedClient.RemoveItemFromList(clientTypedClient.Lists[key], t) > 0;
+            }
+        }
 
+        public static void RemoveListAll<T>(string key, T t)
+        {
+            using (var client = prcm.GetClient())
+            {
+                var clientTypedClient = client.As<T>();
+                clientTypedClient.Lists[key].RemoveAll();
+            }
+        }
+
+        public static void SetExpireList(string key, DateTime dateTime)
+        {
+            using (var client = prcm.GetClient())
+            {
+                client.ExpireEntryAt(key, dateTime);
+            }
+        }
+
+        public static void AddSet<T>(string key, T t)
+        {
+            using (var client = prcm.GetClient())
+            {
+                var clientTypedClient = client.As<T>();
+                clientTypedClient.Sets[key].Add(t);
+            }
+        }
+        public static bool ContainsSet<T>(string key, T t)
+        {
+            using (var client = prcm.GetClient())
+            {
+                var clientTypedClient = client.As<T>();
+                return clientTypedClient.Sets[key].Contains(t);
+            }
+        }
+        public static bool RemoveSet<T>(string key, T t)
+        {
+            using (var client = prcm.GetClient())
+            {
+                var clientTypedClient = client.As<T>();
+                return clientTypedClient.Sets[key].Remove(t);
+            }
+        }
+
+        public static bool ExistHash<T>(string hashId, string key)
+        {
+            using (var client = prcm.GetClient())
+            {
+                return client.HashContainsEntry(hashId, key);
+            }
+        }
+
+        public static bool SetHash<T>(string hashId,string key ,T t)
+        {
+            using (var client = prcm.GetClient())
+            {
+                var value = ServiceStack.Text.JsonSerializer.SerializeToString<T>(t);
+                return client.SetEntryInHash(hashId, key, value);
+            }
         }
     }
 }
