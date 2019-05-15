aa58afeb584111d3839218b558f430385ee17abf
Tangming <infly123@sina.com>
2019-05-15 08:31:10 +0800

    update

--- src/MongoDB/Java/MongoDbDemo/src/main/java/com/tm/MongoDbDemo/MongoDbDemoApplication.java
+++ src/MongoDB/Java/MongoDbDemo/src/main/java/com/tm/MongoDbDemo/MongoDbDemoApplication.java
@@ -1,5 +1,10 @@
 package com.tm.MongoDbDemo;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +13,10 @@ public class MongoDbDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MongoDbDemoApplication.class, args);
+
+		MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017");
+		MongoDatabase database = mongoClient.getDatabase("test");
+		MongoCollection<Document> collection = database.getCollection("restaurants");
 	}
 
 }
