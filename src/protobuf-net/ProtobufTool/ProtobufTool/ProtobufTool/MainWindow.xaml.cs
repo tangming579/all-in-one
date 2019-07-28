8b58097c5d26f76cc2e4c75fb14bc6fc2750d607
Tangming <infly123@sina.com>
2019-07-28 13:43:46 +0800

    proto

--- src/protobuf-net/ProtobufTool/ProtobufTool/ProtobufTool/MainWindow.xaml.cs
+++ src/protobuf-net/ProtobufTool/ProtobufTool/ProtobufTool/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,63 @@ namespace ProtobufTool
 
         private void btnCreate_Click(object sender, RoutedEventArgs e)
         {
+            switch (cobVersion.SelectedIndex)
+            {
+                case 0: txbOutput.Text = protogen(); break;
+                case 1: txbOutput.Text = protoc(); break;
+            }
+        }
 
+        //生成proto3
+        public string protoc()
+        {
+            string command = $@".\protoc.exe --proto_path=protos --csharp_out=gen Demo-Proto3.proto";
+
+            Process pro = new Process();
+            pro.StartInfo.FileName = "cmd.exe";
+
+            pro.StartInfo.CreateNoWindow = true;         // 不创建新窗口    
+            pro.StartInfo.UseShellExecute = false;       //不启用shell启动进程  
+            pro.StartInfo.RedirectStandardInput = true;  // 重定向输入    
+            pro.StartInfo.RedirectStandardOutput = true; // 重定向标准输出    
+            pro.StartInfo.RedirectStandardError = true;
+            // 重定向错误输出  
+            pro.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            pro.Start();//开启cmd
+            pro.StandardInput.WriteLine(command);
+            pro.StandardInput.AutoFlush = true;
+            pro.StandardInput.WriteLine("exit"); //若是运行时间短可加入此命令
+
+            string output = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();//若运行时间长,使用这个,等待程序执行完退出进程
+            pro.Close();
+            return output;
+        }
+
+        //老版protogen生成器，只可生成proto2
+        public string protogen()
+        {
+            string command = $@"ProtoGen\protogen.exe -i:protos\Proto2.proto -o:gen\Proto2.cs";
+
+            Process pro = new Process();
+            pro.StartInfo.FileName = "cmd.exe";
+
+            pro.StartInfo.CreateNoWindow = true;         // 不创建新窗口    
+            pro.StartInfo.UseShellExecute = false;       //不启用shell启动进程  
+            pro.StartInfo.RedirectStandardInput = true;  // 重定向输入    
+            pro.StartInfo.RedirectStandardOutput = true; // 重定向标准输出    
+            pro.StartInfo.RedirectStandardError = true;
+            // 重定向错误输出  
+            pro.StartInfo.WorkingDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            pro.Start();//开启cmd
+            pro.StandardInput.WriteLine(command);
+            pro.StandardInput.AutoFlush = true;
+            pro.StandardInput.WriteLine("exit"); //若是运行时间短可加入此命令
+
+            string output = pro.StandardOutput.ReadToEnd();
+            pro.WaitForExit();//若运行时间长,使用这个,等待程序执行完退出进程
+            pro.Close();
+            return output;
         }
     }
 }
