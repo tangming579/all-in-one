316bda8e258a003313e1e756dceec38dfa3f037f
Tangming <infly123@sina.com>
2019-08-05 20:16:17 +0800

    SqlSugar

---
+++ note/ORM/SqlSugar/SqlSugarWPF/Common/Config.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugarWPF.Common
+{
+    public static class Config
+    {
+        public static string ConnectionString { private set; get; }
+    }
+}
---
+++ note/ORM/SqlSugar/SqlSugarWPF/Common/DBHelper.cs
@@ -0,0 +1,24 @@
+﻿using SqlSugar;
+using SqlSugarWPF.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SqlSugarWPF
+{
+    public static class DBHelper
+    {
+        public static SqlSugarClient DB
+        {
+            get => new SqlSugarClient(new ConnectionConfig()
+            {
+                ConnectionString = Config.ConnectionString,//必填, 数据库连接字符串
+                DbType = DbType.SqlServer,         //必填, 数据库类型
+                IsAutoCloseConnection = true,       //默认false, 时候知道关闭数据库连接, 设置为true无需使用using或者Close操作
+                InitKeyType = InitKeyType.SystemTable    //默认SystemTable, 字段信息读取, 如：该属性是不是主键，是不是标识列等等信息
+            });
+        }
+    }
+}
