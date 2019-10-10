c062bf1b1c066b82737fe850cf4f8a50746e7ce0
Tangming <infly123@sina.com>
2019-10-10 19:35:37 +0800

    Pipelines

--- code-note/InflySocket/SessionBase.cs
+++ code-note/InflySocket/SessionBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 
@@ -10,5 +11,34 @@ namespace InflySocket
         public string EndPoint { get; set; }
 
         public Socket TcpSocket { get; internal set; }
+
+        /// <summary>
+        /// 发送数据
+        /// </summary>
+        /// <param name="buf"></param>
+        public void Send(byte[] buf)
+        {
+            if (buf != null)
+            {
+                TcpSocket.Send(buf);
+            }
+        }
+        /// <summary>
+        /// 获取连接的ip
+        /// </summary>
+        /// <returns></returns>
+        public string GetIp()
+        {
+            IPEndPoint clientipe = (IPEndPoint)TcpSocket.RemoteEndPoint;
+            string _ip = clientipe.Address.ToString();
+            return _ip;
+        }
+        /// <summary>
+        /// 关闭连接
+        /// </summary>
+        public void Close()
+        {
+            TcpSocket.Shutdown(SocketShutdown.Both);
+        }
     }
 }
