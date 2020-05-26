c8b584e7060e3844989a6af8b5e0ee9989bb92f3
Tangming <infly123@sina.com>
2020-05-26 20:14:35 +0800

    Update Program.cs

--- note/Autofac/src/AutoFac/Program.cs
+++ note/Autofac/src/AutoFac/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Autofac;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +9,63 @@ namespace AutoFac
 {
     class Program
     {
+        public class Movie
+        {
+            public string Name { get; set; }
+        }
+        public interface IMovieFinder
+        {
+            List<Movie> FindAll();
+        }
+        public class MPGMovieLister
+        {
+            private readonly IMovieFinder _movieFinder;
+            //增加了构造函数，参数是IMovieFinder对象
+            public MPGMovieLister(IMovieFinder movieFinder)
+            {
+                _movieFinder = movieFinder;
+            }
+
+            public Movie[] GetMPG()
+            {
+                var allMovies = _movieFinder.FindAll();
+                return allMovies.Where(m => m.Name.EndsWith(".MPG")).ToArray();
+            }
+        }
+        public class ListMovieFinder : IMovieFinder
+        {
+            public List<Movie> FindAll()
+            {
+                return new List<Movie>
+                      {
+                          new Movie
+                              {
+                                  Name = "Die Hard.wmv"
+                              },
+                          new Movie
+                              {
+                                  Name = "My Name is John.MPG"
+                              }
+                      };
+            }
+        }
         static void Main(string[] args)
         {
+            var builder = new ContainerBuilder();
+            //注册ListMovieFinder类型，这里的AsImplementedInterfaces表示以接口的形式注册
+            builder.RegisterType<ListMovieFinder>().AsImplementedInterfaces();
+            //注册MPGMovieLister类型
+            builder.RegisterType<MPGMovieLister>();
+            var _container = builder.Build();
 
+            var lister = _container.Resolve<MPGMovieLister>();
+
+            foreach (var movie in lister.GetMPG())
+            {
+                Console.WriteLine(movie.Name);
+            }
+
+            Console.ReadKey();
         }
     }
 }
